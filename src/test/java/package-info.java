d3fb5ee0d8f151aaed96c4d54818371fcb804f35
ShaneKing <github@azolla.org>
2016-03-27 18:00:38 +0800

    Date0

---
+++ src/test/java/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * @(#)package-info.java		Created at 16/3/26
+ *
+ * Copyright (c) ShaneKing All rights reserved.
+ * ShaneKing PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+/**
+ * Created by ShaneKing on 16/3/26.
+ */
